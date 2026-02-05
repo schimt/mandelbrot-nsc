2d3ecfbee8b45b073c4d0caaea1e7236925720fc
schimt <99011593+schimt@users.noreply.github.com>
2026-02-05 14:49:41 +0100

    Add mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,18 @@
+"""
+Mandelbrot Set Generator
+Author : [Nikolai Schmidt Madsen]]
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+    # TODO : Implement the algorithm
