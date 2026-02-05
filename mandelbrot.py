a77b64836c42b7d2f35e8e5c490b0e19a09ec205
schimt <99011593+schimt@users.noreply.github.com>
2026-02-05 15:14:46 +0100

    Implement mandelbrot point function

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,16 +3,26 @@ Mandelbrot Set Generator
 Author : [Nikolai Schmidt Madsen]]
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
+def mandelbrot(c):
     """
-    Example function .
+    Mandelbrot escape-time function.
     Parameters
     ----------
-    x : float
-    Input value
+    c : complex
+        Input complex number
     Returns
     -------
-    float
-    Output value
+    int
+        Iteration count
     """
-    # TODO : Implement the algorithm
+    max_iter = 200
+    z = 0j
+    for n in range(max_iter):
+        z = z*z + c
+        if (z.real*z.real + z.imag*z.imag) > 4.0:
+            return n
+    return max_iter
+
+
+if __name__ == "__main__":
+    assert mandelbrot(0+0j) == 200
